e79b0cd26e7fb85aa8467bdf3e3b6e76bb6df57a
DritaSkeraj <37184676+DritaSkeraj@users.noreply.github.com>
2021-01-19 00:10:56 +0100

    D5-base project structure

---
+++ D5/src/server.js
@@ -0,0 +1,49 @@
+const dotenv = require("dotenv")
+dotenv.config()
+
+const mongoose = require("mongoose")
+const express = require("express")
+const listEndpoints = require("express-list-endpoints")
+const { join } = require("path")
+const cors = require("cors")
+
+const {
+    notFoundHandler,
+    unauthorizedHandler,
+    forbiddenHandler,
+    catchAllHandler,
+  } = require("./errorHandling")
+
+const server = express()
+  
+const productsRouter = require("./services/Products")
+const port = process.env.PORT || 3001
+const publicFolderPath = join(__dirname, "../public")
+
+server.use(cors())
+server.use(express.json())
+server.use(express.static(publicFolderPath))  
+
+server.use("/products", productsRouter)
+
+server.use(notFoundHandler)
+server.use(unauthorizedHandler)
+server.use(forbiddenHandler)
+server.use(catchAllHandler)
+
+console.log(listEndpoints(server))
+
+mongoose.connect(process.env.MONGO_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(
+    server.listen(port, () => {
+        console.log("Running on port ", port)
+    })
+).catch(err => console.log(err))
+
+
+
+
+
+
---
+++ D5/src/services/Products/schema.js
@@ -0,0 +1,10 @@
+const { Schema } = require("mongoose")
+const mongoose = require("mongoose")
+
+const ProductsSchema = new Schema(
+    {
+        // name, description, brand, imageUrl,
+        // price, category, createdAt, updatedAt
+        
+    }
+)
