2b7a96e9b719821e8cfa6019391d48be8d06e326
DritaSkeraj <37184676+DritaSkeraj@users.noreply.github.com>
2021-01-19 00:30:46 +0100

    adds the schema for products & reviews

--- D5/src/services/Products/schema.js
+++ D5/src/services/Products/schema.js
@@ -5,6 +5,47 @@ const ProductsSchema = new Schema(
     {
         // name, description, brand, imageUrl,
         // price, category, createdAt, updatedAt
-        
+        // reviews: comment, rate, createdAt
+        name: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        brand: {
+            type: String,
+        },
+        imageUrl: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: String,
+            required: true
+        },
+        category: {
+            type: String,
+        },
+        reviews: {
+            type: [{
+                comment: {
+                    type: String,
+                    required: true
+                },
+                rate: {
+                    type: Number,
+                    required: true
+                }, 
+                createdAt: {
+                     type: Date, 
+                     default: Date.now 
+                }
+            }],
+            default: []
+        }
     }
 )
+
+module.exports = mongoose.model("Products", ProductsSchema)
