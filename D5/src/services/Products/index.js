b3ae906835e50d83861fd6f3aa0548f270c992a9
DritaSkeraj <37184676+DritaSkeraj@users.noreply.github.com>
2021-01-19 01:59:36 +0100

    adds crud for embedded reviews

--- D5/src/services/Products/index.js
+++ D5/src/services/Products/index.js
@@ -23,7 +23,7 @@ router.get("/", async (req, res, next) => {
       query.criteria,
       query.options.fields
     )
-      .populate("reviews") 
+      .populate("reviews")
       .sort(query.options.sort)
       .skip(query.options.skip)
       .limit(query.options.limit);
@@ -36,49 +36,158 @@ router.get("/", async (req, res, next) => {
 });
 
 router.get("/:id", async (req, res, next) => {
-    try {
-      const product = await ProductsModel.findById(req.params.id).populate(
-        "reviews"
-      ); 
+  try {
+    const product = await ProductsModel.findById(req.params.id).populate(
+      "reviews"
+    );
+    res.status(200).send({ product });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const product = await ProductsModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        runValidators: true,
+        new: true,
+      }
+    );
+    if (product) res.status(200).send({ product });
+    else {
+      const error = new Error(`product with id ${req.params.id} not found`);
+      error.httpStatusCode = 404;
+      next(error);
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const product = await ProductsModel.findByIdAndDelete(req.params.id);
+    if (product) {
       res.status(200).send({ product });
+    } else {
+      const error = new Error(`User ${req.params.id} not found`);
+      error.httpStatusCode = 404;
+      next(error);
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+router.post("/:id/", async (req, res, next) => {
+  try {
+    const comment = req.body.comment;
+    const rate = req.body.rate;
+
+    const reviewToInsert = {
+      comment,
+      rate,
+    };
+
+    const updatedProduct = await ProductsModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $push: {
+          reviews: reviewToInsert,
+        },
+      },
+      {
+        runValidators: true,
+        new: true,
+      }
+    );
+    res.status(201).send(updatedProduct);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+router.get("/:id/reviews", async (req, res, next) => {
+  try {
+    const { reviews } = await ProductsModel.findById(req.params.id);
+    res.status(200).send(reviews);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+router.get("/:id/reviews/:reviewId", async (req, res, next) => {
+  try {
+    const { reviews } = await ProductsModel.findById(req.params.id, {
+      reviews: {
+        $elemMatch: { _id: req.params.reviewId },
+      },
+    });
+
+    if (reviews && reviews.length > 0) {
+      res.send(reviews[0]);
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+router.put("/:id/reviews/:reviewId", async (req, res, next) => {
+    try {
+      const { reviews } = await ProductsModel.findById(req.params.id, {
+        _id: 0,
+        reviews: {
+          $elemMatch: {
+            _id: req.params.reviewId,
+          },
+        },
+      });
+      
+      if(reviews && reviews.length > 0) {
+          const reviewToReplace = {...reviews[0].toObject(), ...req.body}
+          const modifiedReview = await ProductsModel.findOneAndUpdate(
+              {
+                  _id: mongoose.Types.ObjectId(req.params.id),
+                  "reviews._id": mongoose.Types.ObjectId(req.params.reviewId),
+              }, 
+              { $set: { "reviews.$": reviewToReplace }},
+              {
+                  runValidators: true,
+                  new: true
+              }
+          )
+          res.status(200).send(modifiedReview)
+      } else {
+          next()
+      }
     } catch (error) {
       console.log(error);
       next(error);
     }
   });
 
-  router.put("/:id", async (req, res, next) => {
+  router.delete("/:id/reviews/:reviewId", async (req, res, next) => {
     try {
-      const product = await ProductsModel.findByIdAndUpdate(
+      const modifiedReview = await ProductsModel.findByIdAndUpdate(
         req.params.id,
-        req.body,
         {
-          runValidators: true,
-          new: true,
+          $pull: {
+            reviews: { _id: req.params.reviewId },
+          }
         }
       );
-      if (product) res.status(200).send({ product });
-      else {
-        const error = new Error(`product with id ${req.params.id} not found`);
-        error.httpStatusCode = 404;
-        next(error);
-      }
-    } catch (error) {
-      console.log(error);
-      next(error);
-    }
-  });
-
-  router.delete("/:id", async (req, res, next) => {
-    try {
-      const product = await ProductsModel.findByIdAndDelete(req.params.id);
-      if (product) {
-        res.status(200).send({ product });
-      } else {
-        const error = new Error(`User ${req.params.id} not found`);
-        error.httpStatusCode = 404;
-        next(error);
-      }
+      res.status(200).send(modifiedReview);
     } catch (error) {
       console.log(error);
       next(error);
